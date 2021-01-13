b2eefc7c6181a65eef59fd868e97e7f8fecfd8e7
Vanderlei J Kempf <vandercurso@gmail.com>
2021-01-13 11:14:31 -0300

    Criado Function Listar por ID

--- GetProductById/index.js
+++ GetProductById/index.js
@@ -1,14 +1,16 @@
+const { ObjectID } = require ('mongodb');
 const createMongoClient = require ('../shared/mongoClient');
 
 module.exports = async function (context, req) {
+	const { id } = req.params
+	
 	const { client: MongoClient, closeConnectionFn } = await createMongoClient();
 	const Products = MongoClient.collection('products');
-	const res = await Products.find({});
-	const body = await res.toArray();
+	const res = await Products.findOne({ _id: ObjectID(id) });
 
 	closeConnectionFn();
 	context.res = {
-		status: 200,
-		body,
-	}
+			status: 200,
+			body: res,
+		}
 };
